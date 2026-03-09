14396c40fe7fb43872d1eb140a81390a21cd4924
rakib1k7 <rakibraj1k7dz@gmail.com>
2026-03-09 14:15:11 +0600

    Adding search function by Js

--- home.js
+++ home.js
@@ -119,3 +119,14 @@ document.getElementById("closed-btn").addEventListener("click", () => {
 });
 
 loadCards();
+
+document.getElementById("search-btn").addEventListener("click", () => {
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.toLowerCase().trim().toLowerCase();
+
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
+    .then((res) => res.json())
+    .then((data) => {
+      displayCards(data.data);
+    });
+});

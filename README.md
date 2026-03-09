f862f2ca47e59c34e92699291f16a58db5c06c5e
rakib1k7 <rakibraj1k7dz@gmail.com>
2026-03-09 11:40:47 +0600

    added README and Moadal fucntion by JS

---
+++ README.md
@@ -0,0 +1,17 @@
+var is function scoped. It can be re-declared and update and older way to declare variables.
+let is block scoped.It Can be updated but not re-declared in the same scope.It is introduced in ES6.
+Const block scoped like let.It cannot be re-assigned or re-declared.It must be initialized when declared.
+
+
+The spread operator expands elements of an array or object into individual elements, making it easier to copy, merge, or pass values.
+
+
+Map is used to modify or transform each element of an array.It returns a new array of the same length.
+Filter is used to select elements that meet a condition.It returns a new array with filtered elements.
+forEach() is used to run a function on each element.It does not return a new array.
+
+
+An arrow function is a compact way to write functions in JavaScript using => syntax.
+
+
+Template literals are strings written with backticks ( ) that allow variable interpolation (${}) and multi-line strings easily in JavaScript.
--- home.js
+++ home.js
@@ -3,13 +3,70 @@ const allBtn = document.getElementById("all-btn");
 const openBtn = document.getElementById("open-btn");
 const closedBtn = document.getElementById("closed-btn");
 
+loadCardDetails = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayCardDetails(details.data);
+};
+
+const displayCardDetails = (status) => {
+  // console.log(status);
+  const detailsBox = document.getElementById("modalContainer");
+  const displayBox = document.getElementById("displayBox");
+  displayBox.innerHTML = `
+               <div id="displayBox" class=" rounded-xl">
+          <h1 class="font-bold mb-2">${status.title}</h1>
+          <div class="flex gap-3">
+          <p class="bg-green-700 rounded-3xl p-1 px-2 text-white">${status.status}</p>
+          <p class="text-[#64748B]">.${status.author}</p>
+          <p class="text-[#64748B]">.${status.createdAt}</p>
+        </div>
+        <div class="flex gap-3">
+           <button class="border-2 border-red-300 text-red-500 rounded-2xl  p-1 bg-red-100"><i class="fa-solid fa-bug gap-2"></i>${status.type}</button>
+           <button class="border-2 border-yellow-300 text-yellow-500 rounded-2xl p-1 bg-yellow-100"><i class="fa-regular fa-life-ring"></i>${status.helpWanted}</button>
+        </div>
+        <div>
+          <p class="text-[#64748B] mt-5">${status.description}</p>
+        </div>
+        <div class="flex gap-6 mb-3">
+          <div >
+            <p>Assignee:</p>
+            <h3 class="font bold text-2xl">${status.assignee || "Unassigned"}</h3>
+          </div>
+          <div>
+            <p>Priority:</p>
+            <h3 class="font bold p-2 bg-amber-400 rounded-2xl">${status.priority}</h3>
+          </div>
+        </div>
+      </h1>
+      <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn btn-primary">Close</button>
+      </form>
+    </div>
+    </div>
+  `;
+  detailsBox.showModal();
+};
+
 const loadCards = () => {
+  const cardsContainer = document.getElementById("card-container");
+
+  cardsContainer.classList.add("hidden");
+
   LoadingSpinner.classList.remove("hidden");
   LoadingSpinner.classList.add("flex");
+
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
-    .then((data) => displayCards(data.data));
-    LoadingSpinner.classList.add("hidden");
+    .then((data) => {
+      displayCards(data.data);
+
+      LoadingSpinner.classList.add("hidden");
+      cardsContainer.classList.remove("hidden");
+    });
 };
 
 const displayCards = (cards) => {
@@ -20,7 +77,7 @@ const displayCards = (cards) => {
     const cardDiv = document.createElement("div");
 
     cardDiv.innerHTML = `
-           <div  class="shadow-gray-300 shadow-md rounded-box p-3">
+           <div onclick="loadCardDetails(${card.id})"  class="shadow-gray-300 shadow-md rounded-box p-3 ">
           <!-- Issues will be dynamically added here -->
            <div class="flex h-full justify-between items-center">
             <img src="./assets/Open-Status.png" alt="">
@@ -50,7 +107,7 @@ document.getElementById("open-btn").addEventListener("click", () => {
     .then((data) => {
       const openCards = data.data.filter((card) => card.status === "open");
       displayCards(openCards);
-    } );
+    });
 });
 document.getElementById("closed-btn").addEventListener("click", () => {
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
@@ -58,7 +115,7 @@ document.getElementById("closed-btn").addEventListener("click", () => {
     .then((data) => {
       const closedCards = data.data.filter((card) => card.status === "closed");
       displayCards(closedCards);
-    } );
+    });
 });
 
 loadCards();
